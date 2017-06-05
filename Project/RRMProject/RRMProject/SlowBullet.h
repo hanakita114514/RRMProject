20a59db023eb82e51820ca60da0245bbe92ef364
hanakita0310 <tozica18@gmail.com>
2017-06-05 16:28:07 +0900

    変更

--- Project/RRMProject/RRMProject/SlowBullet.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class SlowBullet
-{
-public:
-	SlowBullet();
-	~SlowBullet();
-};
-
--- Project/RRMProject/RRMProject/main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "File.h"
-#include "MapManeger.h"
-#include "Enemy.h"
-#include "Player.h"
-#include "BulletFactory.h"
-#include "HomingBullet.h"
-#include <vector>
